34cb533b01c951e8e293bb82bfb025735ecbd4ce
Chris666 <g.a_cristo42@hotmail.com>
2024-08-20 10:48:06 -0600

    Portfolio

---
+++ README.md
@@ -0,0 +1,19 @@
+# CV Vitae
+
+Este proyecto es un CV Vitae interactivo desarrollado utilizando Astro, Tailwind CSS y Alpine.js.
+
+## Estructura del Proyecto
+
+El proyecto está dividido en varias secciones, cada una presentada en un formato de acordeón para facilitar la navegación. Las secciones incluyen:
+
+1. **Sobre Mí**
+2. **Educación**
+3. **Proyectos**
+4. **Certificaciones**
+5. **Contacto**
+
+## Tecnologías Utilizadas
+
+- **Astro**: Un generador de sitios estáticos moderno que permite el desarrollo de páginas rápidas y optimizadas.
+- **Tailwind CSS**: Un marco de diseño que facilita la creación de interfaces de usuario personalizables y responsivas.
+- **Alpine.js**: Un pequeño framework de JavaScript para añadir interactividad a los componentes de tu página web.
---
+++ src/pages/content.js
@@ -0,0 +1,126 @@
+// Personal Information --> #hero section
+export const name = "Cristopher Garcia Andrade"
+//export const designation = "Desarrollador backend"
+export const location = "Ciudad de Mexico"
+//export const website = "#"
+
+// About
+export const about = "";
+
+// Work Experience --> #work section
+export const work = [
+	/**
+	{
+		from: "2001",
+		to: "Present",
+		role: "Safety Inspector",
+		org: "Quahog Safety Department",
+		org_url: "https://quahog.gov/safety",
+		location: "Quahog, Rhode Island",
+		skills: "Safety Inspection, Risk Assessment, Emergency Response"
+	}
+	**/
+]
+
+// Projects --> #project section
+export const projects = [
+	{
+		frame: "React",
+		title: "Pomodoro Clock Project",
+		description: "Time management tool that helps users break work into intervals separated by short breaks.  It includes components for displaying and controlling the Pomodoro clock. The timer logic and state management are implemented using React hooks and components.",
+		url: "https://chris6six6.github.io/Build-a-25-5-Clock/",
+		//tech: "Safety Equipment, Training Programs, Community Outreach",
+	},
+	{
+		frame: "D3",
+		title: "Interactive Circle of Fifths",
+		description: "Interactive circle of fifths using the D3.js library. The circle of fifths is a useful tool in music theory that displays the relationships between the 12 notes of the chromatic scale.",
+		url: "https://chris6six6.github.io/Circle-of-fifths/"
+	},
+	{
+		frame: "D3",
+		title: "Choropleth Map Visualization Project",
+		description: "choropleth map displays US counties with different fill colors representing education levels. It includes features such as a title, description, legend, and tooltip for interactive exploration of the data.",
+		url: "https://chris6six6.github.io/Choropleth-Map--D3/"
+	},
+	{
+		frame: "Express , Mongoose",
+		title: "URL Shortener Microservice",
+		description: "Microservice for shortening URLs, built with Node.js, users can submit a long URL and receive a unique shortened URL.",
+		url: "https://url-shortener-microservice-xcqr.onrender.com/"
+	},
+	{
+		frame: "Express , Mongoose",
+		title: "User Exercise Tracker",
+		description: "Microservice API for managing users and their exercise logs. It allows for creating users, adding exercises, and retrieving exercise logs with various filtering options.",
+		url: "https://exercise-tracker-a5gl.onrender.com/"
+	},
+	{
+		frame: "Express , MySQL, Sequelize, Pug ",
+		title: "BienesRaices",
+		description: "Estate portal where visitors can create an account, confirm it, and start posting their properties. The portal includes features such as property descriptions, information, maps, property photographs, and a contact form.",
+		url: "https://bienesraices-1oxe.onrender.com/"
+	},
+	{
+		frame: "Express , Mongoose, Handlebars",
+		title: "DevJobs",
+		description: "Recruiters can create an account and start posting job vacancies, while interested programmers can fill out a form, upload their CV (Curriculum Vitae), and contact recruiters.",
+		url: "https://devjobs-6wph.onrender.com/"
+	},
+]
+
+// Education --> #education section
+export const education = [
+	{
+		from: "2021",
+		to: "2024",
+		institute: "ESCOM IPN",
+		course: "Ingenieria en sistemas computacionales",
+		location: "Instituto politecnico nacional"
+	}
+]
+
+// Certifications --> #certificate section
+
+export const certificates = [
+	{
+		issued: "Freecodecamp",
+		title: "Front End Development Libraries",
+		org: "Level up my skills by learning some of the most popular front end libraries in the industry.",
+		url: "https://www.freecodecamp.org/certification/Chris666/front-end-development-libraries",
+	},
+	{
+		issued: "Freecodecamp",
+		title: "Data Visualization",
+		org: "In the Data Visualization Certification, i'll build charts, graphs, and maps to present different types of data with the D3.js library.",
+		url: "https://www.freecodecamp.org/certification/Chris666/data-visualization",
+	},
+	{
+		issued: "Freecodecamp",
+		title: "Relational Database",
+		org: "I will use real developer tools and software including VS Code, PostgreSQL, and the Linux / Unix command line to complete interactive tutorials and build projects.",
+		url: "https://www.freecodecamp.org/certification/Chris666/relational-database-v8",
+	},
+	{
+		issued: "Freecodecamp",
+		title: "Back End Development and APIs",
+		org: "I'll learn how to write back end apps with Node.js and npm. I'll also build web applications with the Express framework, and build a People Finder microservice with MongoDB and the Mongoose library.",
+		url: "https://www.freecodecamp.org/certification/Chris666/back-end-development-and-apis",
+	}
+]
+
+// Contact --> #contact section
+export const contact = [
+	{
+		frame: "E-mail",
+		title: "g.a.cristo42@gmail.com",
+		source_name: "LinkedIn",
+		source: "https://www.linkedin.com/in/chris6six6/",
+	},
+	{
+		frame: "Phone",
+		title: "5521874023",
+		source_name: "Github",
+		source: "https://github.com/Chris6six6",
+	},
+]
